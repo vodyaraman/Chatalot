141d08819451be1aeba3840f208fc70ffea7fcdc
vodyaraman <130937079+vodyaraman@users.noreply.github.com>
2024-10-15 19:26:31 +0300

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,212 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import Image from 'next/image';
+import { useState } from 'react';
+import "./mainpage.scss";
 
-export default function Home() {
+const Header = () => (
+  <header className="header">
+    <div className="header__title">
+      <Image src="/logo.png" alt="Профиль" width={35} height={45} objectFit='contain' className="header__title-logo" />
+      CHATALOT
+    </div>
+    <div className="header__account">
+      <span className="header__username">Джон Доу</span>
+      <Image src="/unknown.png" alt="Профиль" width={45} height={45} className="header__profile-image" />
+    </div>
+  </header>
+);
+
+const Sidebar = () => (
+  <aside className="sidebar">
+    <div className="sidebar__search">
+      <input type="text" placeholder="Поиск" className="sidebar__search-input" />
+    </div>
+    <ul className="sidebar__contacts">
+      <li className="sidebar__contact">Алиса</li>
+      <li className="sidebar__contact">Боб</li>
+      <li className="sidebar__contact">Чарли</li>
+    </ul>
+  </aside>
+);
+
+const Account = () => (
+  <div className="account">
+    <h2 className="account__title">Мой Аккаунт</h2>
+    <div className="account__details">
+      <Image src="/profile.jpg" alt="Профиль" width={100} height={100} className="account__profile-image" />
+      <div className="account__info">
+        <p>Имя: Джон Доу</p>
+        <p>Email: john.doe@example.com</p>
+        <button className="account__edit-button">Редактировать</button>
+      </div>
+    </div>
+  </div>
+);
+
+
+const ChatWindow = () => {
+  const [messages, setMessages] = useState([
+    { id: 1, userId: 'friend', senderName: 'Друг', avatar: '/unknown.png', content: 'Привет! Как дела?', type: 'received' },
+    { id: 2, userId: 'me', senderName: 'Вы', avatar: '/unknown.png', content: 'Хорошо, спасибо!', type: 'sent' },
+  ]);
+
+  const [inputValue, setInputValue] = useState('');
+  const [participants, setParticipants] = useState([
+    { id: 'me', name: 'Вы', avatar: '/unknown.png', isAdmin: true },
+    { id: 'friend', name: 'Друг', avatar: '/unknown.png', isAdmin: false },
+  ]);
+  const [chatTitle, setChatTitle] = useState('');
+  const [isEditingTitle, setIsEditingTitle] = useState(false);
+  const [isManagingParticipants, setIsManagingParticipants] = useState(false);
+
+  const handleSendMessage = () => {
+    if (inputValue.trim() !== '') {
+      const newMessage = {
+        id: messages.length + 1,
+        userId: 'me',
+        senderName: 'Вы',
+        avatar: '/unknown.png',
+        content: inputValue,
+        type: 'sent',
+      };
+      setMessages([...messages, newMessage]);
+      setInputValue('');
+    }
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      handleSendMessage();
+    }
+  };
+
+  const toggleEditTitle = () => {
+    setIsEditingTitle(!isEditingTitle);
+  };
+
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setChatTitle(e.target.value);
+  };
+
+  const toggleManageParticipants = () => {
+    setIsManagingParticipants(!isManagingParticipants);
+  };
+
+  const addParticipant = () => {
+    const newParticipant = {
+      id: `participant_${participants.length + 1}`,
+      name: `Участник ${participants.length + 1}`,
+      avatar: '/unknown.png',
+      isAdmin: false,
+    };
+    setParticipants([...participants, newParticipant]);
+  };
+
+  const removeParticipant = (id: string) => {
+    setParticipants(participants.filter((participant) => participant.id !== id));
+  };
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="chat-window">
+      <div className="chat-window__header">
+        {participants.length > 1 ? (
+          <>
+            {isEditingTitle ? (
+              <input
+                type="text"
+                value={chatTitle}
+                onChange={handleTitleChange}
+                onBlur={toggleEditTitle}
+                className="chat-window__title-input"
+              />
+            ) : (
+              <div className='flex-div'>
+                <h2 className="chat-window__title" onClick={toggleEditTitle}>
+                  {chatTitle || 'Чат с друзьями'}
+                </h2>
+                <button onClick={toggleEditTitle} className="chat-window__edit-title-button">
+                  <Image src="/rename.png" alt="Отправить" width={20} height={20} />
+                </button>
+              </div>
+            )}
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+            <div className='flex-div'>
+              <div className="chat-window__participants">Участников: 
+                {participants.length}
+              </div>
+              <button onClick={toggleManageParticipants} className="chat-window__manage-participants-button">
+                <Image src="/burger.png" alt="Отправить" width={15} height={15} />
+              </button>
+            </div>
+          </>
+        ) : (
+          <h2 className="chat-window__title">Чат с {participants.find((p) => p.id !== 'me')?.name}</h2>
+        )}
+      </div>
+      {isManagingParticipants && (
+        <div className="chat-window__manage-participants">
+          Управление участниками
+          <button onClick={addParticipant} className="add-participant-button">
+            Пригласить
+          </button>
+          <button onClick={toggleManageParticipants} className="close-manage-participants-button">
+            Закрыть
+          </button>
+          <ul className="participants-list">
+            {participants.map((participant) => (
+              <li key={participant.id} className="participant-item">
+                <Image src={participant.avatar} alt={participant.name} width={30} height={30} />
+                <span>
+                  {participant.name}
+                  </span>
+                {!participant.isAdmin && participant.id !== 'me' && (
+                  <button onClick={() => removeParticipant(participant.id)} className="remove-participant-button">
+                    Удалить
+                  </button>
+                )}
+              </li>
+            ))}
+          </ul>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      )}
+      <div className="chat-window__messages">
+        {messages.map((message) => (
+          <div key={message.id} className={`chat-window__message-line--${message.type}`}>
+            <div className="chat-window__message-avatar">
+              <Image src={message.avatar} alt={message.senderName} width={30} height={30} />
+            </div>
+            <div className={`chat-window__message chat-window__message--${message.type}`}>
+              {message.content}
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="chat-window__input">
+        <input
+          type="text"
+          placeholder="Введите сообщение..."
+          className="chat-window__input-field"
+          value={inputValue}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setInputValue(e.target.value)}
+          onKeyUp={handleKeyPress}
+        />
+        <button className="chat-window__input-button" onClick={handleSendMessage}>
+          <Image src="/sendmessage.svg" alt="Отправить" width={25} height={25} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default function ChatApp() {
+  return (
+    <div className="chat-app">
+      <Header />
+      <div className="chat-app__body">
+        <Sidebar />
+        <ChatWindow />
+        <Account />
+      </div>
     </div>
   );
 }
